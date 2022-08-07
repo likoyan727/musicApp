5f3f581d378bcf333830ab999f7bab6218226901
likoyan727 <likoyan21@gmail.com>
2022-08-07 16:25:08 -0700

    Add existing file

---
+++ Artist.java
@@ -0,0 +1,15 @@
+// Liana Ikoyan
+// 002395362
+// ikoyan@chapman.edu
+// CPSC-231-01
+// MP4_MusicApp
+// This class creates an Artist object, extended from the Accounts class
+
+public class Artist extends Accounts {
+
+  // default constructor
+  public Artist() {
+    super();
+  }
+
+}
