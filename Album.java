74f8893e457ba1de0bdcc757d99b7dfad860e01b
likoyan727 <likoyan21@gmail.com>
2022-08-07 15:29:17 -0700

    Add existing file

---
+++ Album.java
@@ -0,0 +1,16 @@
+// Liana Ikoyan
+// 002395362
+// ikoyan@chapman.edu
+// CPSC-231-01
+// MP4_MusicApp
+// This class creates an Album object, extended from the ContentCollections class
+
+public class Album extends ContentCollections {
+
+  // default constructor
+  public Album() {
+    super();
+
+  }
+
+}
